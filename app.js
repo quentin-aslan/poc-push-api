eda7438f712c7f3f6f0efa99a44753a34214496f
Aslan Quentin <quentin.aslan@outlook.com>
2023-07-19 00:36:37 -0400

    :sparkles: Store subscriptions datas with lowdb package

--- app.js
+++ app.js
@@ -2,14 +2,48 @@
 const fs = require('fs')
 const https = require('https')
 const http = require('http')
+const webPush = require('web-push');
+import { Low } from 'lowdb'
+import { JSONFile } from 'lowdb/node'
+
 const express = require('express');
-const webpush = require('web-push');
 const app = express();
 const SERVER_PORT = process.env.PORT || 4000;
+let CURRENT_DB = undefined
 
-app.use(require('body-parser').json());
-app.use(express.static('public'));
-
+// Functions
+const getDb = () => {
+    if (CURRENT_DB) return CURRENT_DB
+    const file = './db.json'
+    if (!fs.existsSync(file)) fs.writeFileSync(file, '')
+    const defaultData = { subscriptions: [], vapidKeys: { public: undefined, privateKey: undefined } }
+    const adapter = new JSONFile(file)
+    const db = new Low(adapter, defaultData)
+    CURRENT_DB = db
+    return db
+}
+const getVapidKeys = async () => {
+    const db = getDb()
+    if (db.data.vapidKeys.publicKey && db.data.vapidKeys.privateKey) return db.data.vapidKeys
+    db.data.vapidKeys = webPush.generateVAPIDKeys()
+    await db.write()
+}
+const initwebPush = async () => {
+    const vapidKeys = await getVapidKeys()
+    webPush.setVapidDetails(
+        'mailto:quentin.aslan@outlook.com',
+        vapidKeys.publicKey,
+        vapidKeys.privateKey
+    )
+}
+const saveSubscriptionInDb = async (subscription) => {
+    const db = getDb()
+    const user = db.data.subscriptions.find(user => user.endpoint === subscription.endpoint)
+    if (!user) {
+        db.data.subscriptions.push(subscription)
+        await db.write()
+    }
+}
 const getCertificate = () => {
     const fullchainPath = './certs/fullchain.pem'
     const privkeyPath = './certs/privkey.pem'
@@ -20,7 +54,6 @@ const getCertificate = () => {
         key: fs.readFileSync('./certs/privkey.pem')
     }
 }
-
 const getServer = () => {
     if (!getCertificate()) {
         console.log('No certificates found, starting http server instead of https')
@@ -31,32 +64,15 @@ const getServer = () => {
     }
 }
 
+// Start server
+app.use(require('body-parser').json());
+app.use(express.static('public'));
 getServer().listen(SERVER_PORT, () => console.log(`Server started on port ${SERVER_PORT}`))
 
-// Generate VAPID Keys
-// const vapidKeys = webpush.generateVAPIDKeys();
+initwebPush()
 
 
-const vapidKeys = {
-    publicKey: 'BIo5NOSDvPB1w69wLIEx2or-QU78-OIbseYO6HfrA9Soz-hZ6tq-DxoASYxoDn0L9Chu2Ile_R5qiMKnb3tZoVM',
-    privateKey: '0hImWJil0riPbQ3OYHt6kW-98jAnc16Drh1avTRLIDc'
-}
-
-console.log('Vapid Keys')
-
-
-// Set VAPID Details
-webpush.setVapidDetails(
-    'mailto:quentin.aslan@outlook.com', // Your email
-    vapidKeys.publicKey,   // Your public VAPID key
-    vapidKeys.privateKey   // Your private VAPID key
-);
-
-// Handle subscription
-
-let savedSubscription;
-
-// Create a api route who send index.html
+// API ROUTES
 
 app.get('/', (req, res) => {
     res.status(200).sendFile('index.html');
@@ -66,28 +82,44 @@ app.get('/vapid_public', (req, res) => {
     res.status(200).json({vapid_public_key: vapidKeys.publicKey})
 })
 
-app.post('/subscribe', (req, res) => {
-    savedSubscription = req.body;
-    console.log('subscribe user ...')
-    console.log(savedSubscription)
-    res.status(201).json({});
+app.post('/subscribe', async (req, res) => {
+    try {
+        const subscription = req.body;
+        // Check if subscription have all keys
+        if (!subscription.endpoint || !subscription.keys) {
+            res.status(400).json({ message: 'Subscription must have an endpoint and keys' });
+        }
+
+        await saveSubscriptionInDb(subscription)
+        res.status(201).json({ message: 'Subscription added successfully.' });
+    } catch (e) {
+        res.status(500).json({ message: 'Error when saving the subscription.' });
+    }
 });
 
 
 // Send a push notification
 app.post('/sendNotification', (req, res) => {
-    const notificationPayload = {
-        notification: {
-            title: 'New Notification',
-            body: 'This is the body of the notification',
-            icon: 'icon.png',
-        },
-    };
+    try {
+        const notificationPayload = {
+            notification: {
+                title: 'New Notification',
+                body: 'This is the body of the notification',
+                icon: 'icon.png',
+            },
+        };
 
-    webpush.sendNotification(
-        savedSubscription,
-        JSON.stringify(notificationPayload)
-    );
+        const subscriptions = getDb().data.subscriptions
+        subscriptions.forEach(subscription => {
+            webPush.sendNotification(
+                subscription,
+                JSON.stringify(notificationPayload)
+            );
+        })
 
-    res.sendStatus(200)
-});
+        res.status(201).json({ message: 'Notification sent successfully.' })
+    } catch (e) {
+        console.error(e)
+        res.status(500).json({ message: 'Error when sending the notification.' });
+    }
+})
